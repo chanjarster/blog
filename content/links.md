1a6eeb04c554c4468826852862b52b5b8a757836
Daniel Qian <chanjarster@gmail.com>
2018-08-23 16:30:24 +0800

    change theme

---
+++ content/links.md
@@ -0,0 +1,8 @@
+---
+title: "Links"
+date: 2018-08-23T16:00:03+08:00
+layout: "links"
+menu: "main"
+weight: 40
+---
+
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 # if using a theme, replace with `hugo -t <YOURTHEME>`
 # hugo 
-hugo -t ananke
+hugo -t hugo-nuo
 
 # Go To Public folder
 cd public
